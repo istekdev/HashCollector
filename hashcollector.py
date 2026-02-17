5473ac3f35314b8e25c9812b162a06e586193c10
ISTEK <200693214+istekdev@users.noreply.github.com>
2026-02-17 00:24:40 -0600

    Update hashcollector.py

--- hashcollector.py
+++ hashcollector.py
@@ -6,7 +6,7 @@ from pathlib import Path
 
 with open("config.json", "r") as r:
   config = json.load(r)
-with open(config["directories"]["hashTemplate"], "r") as r:
+with open(config['directories']['hashTemplate'], "r") as r:
   hashEntry = json.load(r)
 hashEntry = copy.deepcopy(hashEntry)
 
@@ -29,45 +29,45 @@ def tobyte(inp):
     return str(inp).encode("utf-8")
 
 def new(algo, source, hash, comment):
-  hashEntry["metadata"]["timestamp"], hashEntry["entry"]["algo"], hashEntry["entry"]["source"], hashEntry["entry"]["hash"], hashEntry["comments"] = round(time.time()), algo, source, hash, comment
-  if not os.path.exists(config["directories"]["hashEntries"]):
-    hashEntry["metadata"]["prevHash"] = "0"*64
-    hashEntry["metadata"]["entryHash"] = sha256(sha256(tobyte(hashEntry["metadata"]["prevHash"]) + tobyte(hashEntry["metadata"]["timestamp"]) + tobyte(hashEntry["metadata"]["index"]) + tobyte(hashEntry["entry"]["algo"]) + tobyte(hashEntry["entry"]["source"]) + tobyte(hashEntry["entry"]["hash"]) + tobyte(hashEntry["comments"])).digest()).hexdigest()
+  hashEntry['metadata']['timestamp'], hashEntry['entry']['algo'], hashEntry['entry']['source'], hashEntry['entry']['hash'], hashEntry['comments'] = round(time.time()), algo, source, hash, comment
+  if not os.path.exists(config['directories']['hashEntries']):
+    hashEntry['metadata']['prevHash'] = "0"*64
+    hashEntry['metadata']['entryHash'] = sha256(sha256(tobyte(hashEntry['metadata']['prevHash']) + tobyte(hashEntry['metadata']['timestamp']) + tobyte(hashEntry['metadata']['index']) + tobyte(hashEntry['entry']['algo']) + tobyte(hashEntry['entry']['source']) + tobyte(hashEntry['entry']['hash']) + tobyte(hashEntry['comments'])).digest()).hexdigest()
     db = [hashEntry]
   else:
-    with open(config["directories"]["hashEntries"], "r") as r:
+    with open(config['directories']['hashEntries'], "r") as r:
       db = json.load(r)
-    hashEntry["metadata"]["prevHash"] = db[-1]["metadata"]["entryHash"]
-    hashEntry["metadata"]["index"] = len(db) + 1
-    hashEntry["metadata"]["entryHash"] = sha256(sha256(tobyte(hashEntry["metadata"]["prevHash"]) + tobyte(hashEntry["metadata"]["timestamp"]) + tobyte(hashEntry["metadata"]["index"]) + tobyte(hashEntry["entry"]["algo"]) + tobyte(hashEntry["entry"]["source"]) + tobyte(hashEntry["entry"]["hash"]) + tobyte(hashEntry["comments"])).digest()).hexdigest()
+    hashEntry['metadata']['prevHash'] = db[-1]['metadata']['entryHash']
+    hashEntry['metadata']['index'] = len(db) + 1
+    hashEntry['metadata']['entryHash'] = sha256(sha256(tobyte(hashEntry['metadata']['prevHash']) + tobyte(hashEntry['metadata']['timestamp']) + tobyte(hashEntry['metadata']['index']) + tobyte(hashEntry['entry']['algo']) + tobyte(hashEntry['entry']['source']) + tobyte(hashEntry['entry']['hash']) + tobyte(hashEntry['comments'])).digest()).hexdigest()
     db.append(hashEntry)
-  with open(config["directories"]["hashEntries"], "w") as w:
-    if config["configs"]["compressed"]:
+  with open(config['directories']['hashEntries'], "w") as w:
+    if config['configs']['compressed']:
       json.dump(db, w, separators=(",", ":"))
     else:
       json.dump(db, w, indent=4)
 
 def view(index):
-  with open(config["directories"]["hashEntries"], "r") as r:
+  with open(config['directories']['hashEntries'], "r") as r:
     db = json.load(r)
   for entries in db:
     if entries.get("metadata", {}).get("index") == int(index):
-      return f"{str(entries["metadata"]["index"])}<?>{str(entries["metadata"]["timestamp"])}<?>{entries["entry"]["hash"]}<?>{entries["entry"]["algo"]}<?>{entries["entry"]["source"]}<?>{entries["comments"]}"
+      return f"{str(entries['metadata']['index'])}<?>{str(entries['metadata']['timestamp'])}<?>{entries['entry']['hash']}<?>{entries['entry']['algo']}<?>{entries['entry']['source']}<?>{entries['comments']}"
 
 def verify():
-  if not os.path.exists(config["directories"]["hashEntries"]):
+  if not os.path.exists(config['directories']['hashEntries']):
     return None
   else:
-    with open(config["directories"]["hashEntries"], "r") as r:
+    with open(config['directories']['hashEntries'], "r") as r:
       db = json.load(r)
     if len(db) == 1:
-      if db[-1]["metadata"]["prevHash"] == "0"*64:
+      if db[-1]['metadata']['prevHash'] == "0"*64:
         return True
       else:
         return False
     else:
       for entries in range(1, len(db)):
-        if db[entries]["metadata"]["prevHash"] == db[entries-1]["metadata"]["entryHash"]:
+        if db[entries]['metadata']['prevHash'] == db[entries-1]['metadata']['entryHash']:
           pass
         else:
           return False
@@ -78,7 +78,7 @@ def menu():
   if val or val == None:
     pass
   else:
-    message_dialog(title="Fatal Error", text=f"This Error is Due To Possible Tampering Detected In '{config["directories"]["hashEntries"]}'. Exiting in 3 Seconds.").run()
+    message_dialog(title="Fatal Error", text=f"This Error is Due To Possible Tampering Detected In '{config['directories']['hashEntries']}'. Exiting in 3 Seconds.").run()
     time.sleep(3)
     sys.exit(1)
   moptions = button_dialog(title="HashCollector - Menu", text="Select An Option", buttons=[("New Hash", 1), ("View Hashes", 2), ("Settings", 3), ("Exit", 4)]).run()
@@ -105,12 +105,12 @@ def menu():
     menu()
   elif moptions == 2:
     t = "HashCollector - View Hashes"
-    if not os.path.exists(config["directories"]["hashEntries"]):
+    if not os.path.exists(config['directories']['hashEntries']):
       message_dialog(title=t, text="No Existing Hashes Yet.").run()
       time.sleep(1)
       menu()
     else:
-      with open(config["directories"]["hashEntries"], "r") as r:
+      with open(config['directories']['hashEntries'], "r") as r:
         db = json.load(r)
       indexes = []
       for entries in db:
@@ -128,28 +128,28 @@ def menu():
     t = "HashCollector - Settings"
     soptions = button_dialog(title=t, text=None, buttons=[("Data Compression", 1), ("Information", 2), ("Exit", 3)]).run()
     if soptions == 1:
-      if config["configs"]["compressed"]:
+      if config['configs']['compressed']:
         soption = button_dialog(title=t, text="Do You Want To Disable DB Compression?", buttons=[("Yes", 1), ("No", 2)]).run()
         if soption == 1:
-          config["configs"]["compressed"] = False
+          config['configs']['compressed'] = False
           with open("config.json", "w") as w:
             json.dump(config, w, indent=4)
         else:
           menu()
-      elif config["configs"]["compressed"] == False:
+      elif config['configs']['compressed'] == False:
         soption = button_dialog(title=t, text="Do You Want To Enable DB Compression?", buttons=[("Yes", 1), ("No", 2)]).run()
         if soption == 1:
-          config["configs"]["compressed"] = True
+          config['configs']['compressed'] = True
           with open("config.json", "w") as w:
             json.dump(config, w, indent=4)
         else:
           menu()
     elif soptions == 2:
-      message_dialog(title=t, text=f"Version: {str(config["metadata"]["version"])}\nDeveloper: {config["metadata"]["developer"]}\nGithub: {config["metadata"]["github"]}\nCreated: {datetime.fromtimestamp(config["metadata"]["created"])}").run()
+      message_dialog(title=t, text=f"Version: {str(config['metadata']['version'])}\nDeveloper: {config['metadata']['developer']}\nGithub: {config['metadata']['github']}\nCreated: {datetime.fromtimestamp(config['metadata']['created'])}").run()
       menu()
   elif moptions == 4:
     sys.exit(0)
 
-folder, file = config["directories"]["hashEntries"].split("/")
+folder, file = config['directories']['hashEntries'].split("/")
 os.makedirs(folder, exist_ok=True)
 menu()
