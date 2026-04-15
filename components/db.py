ab162302cbbf3e2676968ac666a2a16cd7b01dde
ISTEK <200693214+istekdev@users.noreply.github.com>
2026-04-14 21:06:26 -0500

    Create db.py

---
+++ components/db.py
@@ -0,0 +1,41 @@
+from components.toolbox import byte, localsave, config
+from components.crypto import argon, aes, sign
+import plyvel, time, os, json, re
+from hashlib import sha256
+from pathlib import Path
+
+conf = config()
+
+class database:
+  def __init__(self, password):
+    self.enc = aes(password)
+    self.format = re.compile(r"^[A-Za-z0-9+/]{24}:\d+$")
+
+  def new(self, hashingAlgo, source, hash):
+    counter = 0
+    for otherfiles in Path(localsave() / "bin" / "hashes").iterdir():
+      checksum, index = otherfiles.name.split(":")
+      if base64.b64encode(sha256(byte(hash)).digest()[:16]).decode() == checksum:
+        return False
+      if otherfiles.is_dir() and self.format.fullmatch(otherfiles.name):
+        counter += 1
+
+    dat = plyvel.DB(localsave() / "bin" / "hashes" / f"{base64.b64encode(sha256(byte(hash)).digest()[:16]).decode()}_{str(counter + 1)}", create_if_missing=False)
+    dat.put(b"metadata:timestamp", enc.encrypt(byte(round(time.time()))))
+    dat.put(b"hashData:algo", enc.encrypt(byte(hashingAlgo)))
+    dat.put(b"hashData:source", enc.encrypt(byte(source)))
+    dat.put(b"hashData:hash", enc.encrypt(byte(hash)))
+    dat.close()
+    return True
+
+  def view(self, index):
+    filename = None
+    for otherfiles in Path(localsave() / "bin" / "hashes").iterdir():
+      if otherfiles.name.split(":")[1] != index:
+        return False
+      filename = otherfiles.name
+    if enc.verify() == False:
+      return False
+
+    dat = plyvel.DB(localsave() / "bin" / "hashes" / filename, create_if_missing=False)
+    return bytearray(enc.decrypt(dat.get(b"metadata:timestamp"))), bytearray(enc.decrypt(dat.get(b"hashData:algo"))), bytearray(enc.decrypt(dat.get(b"hashData:source"))), bytearray(enc.decrypt(dat.get(b"hashData:hash")))
